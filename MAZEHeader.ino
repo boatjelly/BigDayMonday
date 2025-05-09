107c99c9488a240de4cbc9f5ee89f6b7013d2a2a
Robert G. <guziecr@dupage.edu>
2025-05-08 21:44:23 -0500

    Add files via upload

--- MAZEHeader.ino
+++ MAZEHeader.ino
@@ -3,25 +3,25 @@
   By: Sasha Dauz, Robert J. Guziec, Jacob JM Horstman
   Written: --
   Edited: May 08, 2025
-  Desc: Updated for maze 
+  Desc: Updated for maze to work with LCD screen!
   I/O Pins
-  A0: MUX a
-  A1: MUX b
-  A2: MUX c
-  A3: MUX d
+  A0: LCD DB4
+  A1: LCD DB5
+  A2: LCD DB6
+  A3: LCD DB7
   A4: Right Encoder          [PCI PORTC 0x10]
   A5: Left Encoder           [PCI PORTC 0x20]
-  D0: MUX e
-  D1: MUX f
-  D2: MUX g
+  D0: 
+  D1: 
+  D2: 
   D3: Right Motor Reverse    [TCNT2 OC2B] [Pin 10 H-Bridge]
   D4: Left Motor Control     [x]          [Pin 1 H-Bridge]
   D5: Left Motor Reverse     [TCNT0 OC0B] [Pin 7 H-Bridge]
   D6: Left Motor Forward     [TCNT0 OC0A] [Pin 2 H-Bridge]
   D7: Right Motor Control    [x]          [Pin 9 H-Bridge]
-  D8: MUX cathode 4
-  D9: MUX cathode 3
-  D10: MUX cathode 2
+  D8: LCD R/S
+  D9: LCD RW
+  D10: E
   D11: Right Motor Forward   [TCNT2 OC2A] [Pin 15 H-Bridge]
   D12: Right Whisker         [PCI PORTB 0x10]
   D13: Left Whisker          [PCI PORTB 0x20]
