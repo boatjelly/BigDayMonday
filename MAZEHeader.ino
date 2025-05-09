fb89d4f27c5e5d4d629dfd247993044c2231b3e8
Robert G. <guziecr@dupage.edu>
2025-05-08 20:51:29 -0500

    Add files via upload

---
+++ MAZEHeader.ino
@@ -0,0 +1,28 @@
+/*
+  Smart Car Permanent Header
+  By: Sasha Dauz, Robert J. Guziec, Jacob JM Horstman
+  Written: --
+  Edited: May 08, 2025
+  Desc: Updated for maze 
+  I/O Pins
+  A0: MUX a
+  A1: MUX b
+  A2: MUX c
+  A3: MUX d
+  A4: Right Encoder          [PCI PORTC 0x10]
+  A5: Left Encoder           [PCI PORTC 0x20]
+  D0: MUX e
+  D1: MUX f
+  D2: MUX g
+  D3: Right Motor Reverse    [TCNT2 OC2B] [Pin 10 H-Bridge]
+  D4: Left Motor Control     [x]          [Pin 1 H-Bridge]
+  D5: Left Motor Reverse     [TCNT0 OC0B] [Pin 7 H-Bridge]
+  D6: Left Motor Forward     [TCNT0 OC0A] [Pin 2 H-Bridge]
+  D7: Right Motor Control    [x]          [Pin 9 H-Bridge]
+  D8: MUX cathode 4
+  D9: MUX cathode 3
+  D10: MUX cathode 2
+  D11: Right Motor Forward   [TCNT2 OC2A] [Pin 15 H-Bridge]
+  D12: Right Whisker         [PCI PORTB 0x10]
+  D13: Left Whisker          [PCI PORTB 0x20]
+*/
