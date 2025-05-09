6be3abc81045a8922689e282768625dd102d76e5
horstmanj34 <horstmanj34@dupage.edu>
2025-05-08 20:31:37 -0500

    Add files via upload

---
+++ Header.ino
@@ -0,0 +1,27 @@
+/*
+  Smart Car Permanent Header
+  By: Sasha Dauz, Robert J. Guziec, Jacob JM Horstman
+  Written: --
+  Edited: --
+  I/O Pins
+  A0: Left Sensor            [ADMUX 00]
+  A1: Center Sensor          [ADMUX 01]
+  A2: Right Sensor           [ADMUX 02]
+  A3:
+  A4: Right Encoder          [PCI PORTC 0x10]
+  A5: Left Encoder           [PCI PORTC 0x20]
+  D0:
+  D1:
+  D2:
+  D3: Right Motor Reverse    [TCNT2 OC2B]
+  D4: Left Motor Control     [x]
+  D5: Left Motor Reverse     [TCNT0 OC0B]
+  D6: Left Motor Forward     [TCNT0 OC0A]
+  D7: Right Motor Control    [x]
+  D8: 
+  D9:  
+  D10: 
+  D11: Right Motor Forward   [TCNT2 OC2A]
+  D12: Right Whisker         [PCI PORTB 0x10]
+  D13: Left Whisker          [PCI PORTB 0x20]
+*/
