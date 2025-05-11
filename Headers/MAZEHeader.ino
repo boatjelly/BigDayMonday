caee57908c6e27294635ee0eaafe0d127a72940f
Robert G. <guziecr@dupage.edu>
2025-05-11 15:44:04 -0500

    Update MAZEHeader.ino
    Updated LCD RS and took out enable pins

--- Headers/MAZEHeader.ino
+++ Headers/MAZEHeader.ino
@@ -1,28 +1,28 @@
 /*
   Smart Car Permanent Header
+  Left
   By: Sasha Dauz, Robert J. Guziec, Jacob JM Horstman
-  Written: --
-  Edited: May 08, 2025
-  Desc: Updated for maze to work with LCD screen!
+  Written: May 8th, 2025
+  Edited: --
   I/O Pins
   A0: LCD DB4
-  A1: LCD DB5
-  A2: LCD DB6
-  A3: LCD DB7
+  A1: LCD DB3
+  A2: LCD DB2
+  A3: LCD DB1
   A4: Right Encoder          [PCI PORTC 0x10]
   A5: Left Encoder           [PCI PORTC 0x20]
-  D0: 
-  D1: 
-  D2: 
-  D3: Right Motor Reverse    [TCNT2 OC2B] [Pin 10 H-Bridge]
-  D4: Left Motor Control     [x]          [Pin 1 H-Bridge]
+  D0:
+  D1:
+  D2:
+  D3: 
+  D4: 
   D5: Left Motor Reverse     [TCNT0 OC0B] [Pin 7 H-Bridge]
   D6: Left Motor Forward     [TCNT0 OC0A] [Pin 2 H-Bridge]
-  D7: Right Motor Control    [x]          [Pin 9 H-Bridge]
-  D8: LCD R/S
-  D9: LCD E
-  D10: 
-  D11: Right Motor Forward   [TCNT2 OC2A] [Pin 15 H-Bridge]
+  D7: LCD RS
+  D8: 
+  D9:  Right Motor Forward   [TCNT1 OC1A] [Pin 15 H-Bridge]
+  D10: Right Motor Reverse    [TCNT1 OC1B] [Pin 10 H-Bridge]
+  D11: LCD E
   D12: Right Whisker         [PCI PORTB 0x10]
   D13: Left Whisker          [PCI PORTB 0x20]
 */
